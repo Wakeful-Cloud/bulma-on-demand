c251e7b50dedd19fd4824608668a437e1b708b58
wakeful-cloud <wakefulcloud@gmail.com>
2022-07-11 13:40:50 -0600

    :bug: Vercel fix

--- scripts/build-vercel.sh
+++ scripts/build-vercel.sh
@@ -1,10 +1,11 @@
 # Build for Vercel
 
 # Options
-TARGET=".vercel/output/functions/api/bulma[suffix].func/node_modules/sass-embedded/dist/lib/src/vendor"
+SOURCE="node_modules/sass-embedded/dist"
+TARGET=".vercel/output/functions/api/bulma[suffix].func/node_modules/sass-embedded/dist"
 
 # Build normally
 npm run build
 
-# Copy to the build
-cp node_modules/sass-embedded/dist/lib/src/vendor/dart-sass-embedded $TARGET -R
+# Copy SASS embedded
+cp $SOURCE $TARGET -R
