1c240c4e69ceb132fcc0a298757f99519ea76143
wakeful-cloud <wakefulcloud@gmail.com>
2022-07-11 18:26:04 -0600

    :bug: Vercel debug

--- scripts/build-vercel.sh
+++ scripts/build-vercel.sh
@@ -8,4 +8,12 @@ TARGET=".vercel/output/functions/api/bulma[suffix].func/node_modules/sass-embedd
 npm run build
 
 # Copy SASS embedded
+ls $SOURCE
+ls $TARGET
+
 cp $SOURCE $TARGET -rf
+
+echo "Copied from $SOURCE to $TARGET"
+
+ls $SOURCE
+ls $TARGET
