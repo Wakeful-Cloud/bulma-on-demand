2dc82a617de15c72cf615f85464246b84c0459ec
Wakeful-Cloud <38930607+Wakeful-Cloud@users.noreply.github.com>
2022-11-21 16:36:30 -0700

    🐛 Vercel fix?

--- scripts/build-vercel.sh
+++ scripts/build-vercel.sh
@@ -3,12 +3,12 @@
 # Build for Vercel
 
 # Options
-SOURCE="node_modules/sass-embedded"
+SOURCE="node_modules/sass-embedded-*"
 TARGET=".vercel/output/functions/api/bulma[suffix].func/node_modules"
 
 # Build normally
 npm run build
 
-# Copy SASS embedded
+# Copy the SASS embedded binary
 cp $SOURCE $TARGET -rf
 echo "Copied from $SOURCE to $TARGET"
